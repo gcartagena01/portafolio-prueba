acbed169a3e32d0c8f64ca3bd2a43192c135db0d
Gabriel Cartagena <gcartagena01@hotmail.com>
2020-08-21 01:11:14 -0600

    Servicio Informacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { AcercadeComponent } from './pages/acercade/acercade.component';
 import { ItemComponent } from './pages/item/item.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -20,7 +24,8 @@ import { ItemComponent } from './pages/item/item.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
